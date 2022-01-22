2cf343afcc8357093cac6763ca73a6ec6b5f116c
akash-611 <akashfalcon611@gmail.com>
2022-01-22 20:01:52 +0530

    final update

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # farmdefence
+    Hey guys!
+    This is my first project.
+    I had a really hard time installing the opencv library
+    So I am giving the step by step commands to get that thing in your RPi
+
+    ##__THE COMMANDS ARE HERE AS FOLLOWS:__
+    
+    >sudo apt-get update && sudo apt-get upgrade
+    >sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-100
+    >sudo apt-get install build-essential cmake pkg-config
+    >sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
+    >sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+    >sudo apt-get install libxvidcore-dev libx264-dev
+    >sudo apt-get install libfontconfig1-dev libcairo2-dev
+    >sudo apt-get install libgdk-pixbuf2.0-dev libpango1.0-dev
+    >sudo apt-get install libgtk2.0-dev libgtk-3-dev
+    >sudo apt-get install libatlas-base-dev gfortran
+    >sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-103
+    >sudo apt-get install libqt5gui5 libqt5webkit5 libqt4-test python3-pyqt5
+    >sudo apt-get install python3-dev
+    >sudo apt-get install libatlas-base-dev
+    >sudo apt-get install libjasper-dev
+    >wget https://bootstrap.pypa.io/get-pip.py
+    >sudo python3 get-pip.py
+    >sudo pip3 install opencv-contrib-python
--- finishd.py
+++ finishd.py
@@ -7,6 +7,29 @@ import spidev # To communicate with SPI devices
 from numpy import interp  # To scale values
 from time import sleep  
 import constants as keys
+from picamera import PiCamera
+import datetime
+import cv2
+import os
+
+classNames = []
+classFile = "/home/pi/Documents/farmdefence/coco.names"
+with open(classFile,"rt") as f:
+    classNames = f.read().rstrip("\n").split("\n")
+
+#This is to pull the information about what each object should look like
+configPath = "/home/pi/Documents/farmdefence/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "/home/pi/Documents/farmdefence/frozen_inference_graph.pb"
+
+#This is some set up values to get good results
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+animals = ['bird','cat','dog','horse','sheep','cow','elephant','bear','zebra','giraffe']
+
 
 
 GPIO.cleanup()
@@ -29,12 +52,43 @@ GPIO.setup(fire,GPIO.IN)
 GPIO.setup(pir,GPIO.IN)
 GPIO.setup(led,GPIO.OUT)
 
+GPIO.output(motor,1)
+
+def getObjects(img, thres, nms, draw=True, objects=[]):
+    classIds, confs, bbox = net.detect(img,confThreshold=thres,nmsThreshold=nms)
+#Below has been commented out, if you want to print each sighting of an object to the console you can uncomment below     
+    #print(classIds,bbox)
+    if len(objects) == 0: objects = classNames
+    objectInfo =[]
+    if len(classIds) != 0:
+        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            className = classNames[classId - 1]
+            if className in animals:
+                
+                objectInfo.append([box,className])
+                if (draw):
+                    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                    cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                return className, img
+            
+    
+    return False,img
+
+def TelegramImg(t,name):
+    print(t)
+    for i in keys.CHAT_ID:
+
+        updater.bot.sendPhoto(i,open(name,'rb'),caption = t)
 
 def IrrigationMotor(): 
     print("irrigation started")
-    GPIO.output(motor,1)
-    sleep(3)
     GPIO.output(motor,0)
+    sleep(3)
+    GPIO.output(motor,1)
+    print("irrigation Ended")    
 
 def Blinking_led_and_Buzzer(i): #input "i" is bcm(gpio) pin number of led, declair gpio pin in function
     print("led blinking begins")
@@ -96,7 +150,6 @@ def watermoniteringfunction():
 
 
 def Fire_detection():
-
     i = 0
     print("fire detection started")
     print("Fire Alert")
@@ -111,28 +164,61 @@ def Fire_detection():
             t = "Fire Extinguished"
             TelegramBot(t)
 
-def Motion_detect():
-    
+def videoanalyzer():  
+    cap = cv2.VideoCapture(0)
+    print("camera called")
+    cap.set(3,640)
+    cap.set(4,480)
+    #cap.set(10,70)
+    #Below is the never ending loop that determines what will happen when an object is identified.    
+    k = 0
+    while k < 100:
+        success, img = cap.read()
+        print("image read")
+#Below provides a huge amount of controll. the 0.45 number is the threshold number, the 0.2 number is the nms number)
+        result,img = getObjects(img,0.45,0.2)
+        k += 1
+        if result:
+            t = "{} animal detected".format(result)
+            name = 'spot1.jpg'
+            print(t)
+            cv2.imwrite(name,img)
+            print("image written")
+            TelegramImg(t,name)
+        #print(objectInfo)
+            #cv2.imshow("Output",img)            
+            #cv2.waitKey(5000)
+            os.remove('spot1.jpg')
+            print("imagedeleted")
+            cap.release()
+            break
+
+def Motion_detect():   
     print("motion detection started")
     t = "intruder detected"
-    TelegramBot(t)
+    videoanalyzer()
     Blinking_led_and_Buzzer(led)
 
 updater = Updater(keys.API_KEY, use_context=True)
 dp = updater.dispatcher
-
+i = 0
 while True:
+    i += 1
     moisture = watermoniteringfunction()
+    print(i)
     print(moisture)
     if not GPIO.input(fire):
         Fire_detection()
     elif GPIO.input(pir):
         Motion_detect()
-    elif moisture < 30:
-         IrrigationMotor()
-         TelegramBot("irrigation started")
+    elif moisture < 30 and i%1000==0:
+        TelegramBot("Automated Irrigation Started")
+        IrrigationMotor()
+        TelegramBot("Automated Irrigation Completed")
+        i = 0
     else:
         Mainbotfunction()
+
 
 
       
